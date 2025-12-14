b2fd22b649cfd93adb1cf3a9aef51c302a7a27c7
GalekWorld <u6308111528@gmail.com>
2025-12-14 04:56:53 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,17 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
+// Componente Social Bar
+function SocialBar() {
+  return (
+    <script
+      type="text/javascript"
+      src="https://pl28253981.effectivegatecpm.com/cd/63/35/cd63352fbcabc3905ef613d6c57f4cb4.js"
+      async
+    />
+  );
+}
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -17,15 +28,15 @@ export const metadata: Metadata = {
   title: "ConvertPro",
   description: "Convert your files! Jpg a pdf, Mp4 a Mp3 and more.",
   keywords: ["Next.js", "TypeScript", "Tailwind CSS", "shadcn/ui", "React"],
-  authors: [{ name: "Z.ai Team" }],
+  authors: [{ name: "Dym Team" }],
   icons: {
     icon: "https://i.imgur.com/9MysNF8.jpeg",
   },
   openGraph: {
     title: "ConvertPro",
     description: "Convert your files! Jpg a pdf, Mp4 a Mp3 and more.",
-    url: "sps://chat.z.ai",
-    siteName: "Z.ai",
+    url: "convertidor-archivos.vercel.app",
+    siteName: "ConvertidorArchivos",
     type: "website",
   },
   twitter: {
@@ -42,6 +53,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <SocialBar />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
