8abadf2526e8146b573928b9b478b53b97c625f2
GalekWorld <u6308111528@gmail.com>
2025-12-14 04:45:20 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
-import Advertisement from "@/components/advertisement";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -47,7 +46,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
         {children}
-        <Advertisement />
         <Toaster />
       </body>
     </html>
